494d1a0d5c5cefcf0e6ab84e2c13928c86db1094
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-19 21:05:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 //your JS code here. If required.
-let firstnum=prompt("Enter first number");
-let secondnum=prompt("Enter second number");
+let firstNumber = prompt("Enter the first number:");
+let secondNumber = prompt("Enter the second number:");
 
-if(!isNaN(firstnum) && !isNaN(secondnum)){
-	let sum=Number(firstnum)+Number(secondnum);
-	aler('The sum of ${firstnum} and ${secondnum} is ${sum}');
+// Check if both inputs are numbers
+if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+  let sum = Number(firstNumber) + Number(secondNumber);
+  alert(`The sum of ${firstNumber} and ${secondNumber} is ${sum}`);
+} 
+// Check if only one input is a number
+else if (!isNaN(firstNumber) || !isNaN(secondNumber)) {
+  alert("Invalid input. Please enter a valid number.");
 }
-elseif(!isNaN(firstnum) || !isNaN(secondnum))
-{
-	alert('Invalid input. Please enter a valid number.');
+// Handle invalid input (non-numerical values)
+else {
+  alert("Invalid input. Please enter a valid number.");
 }
-else{
-	alert('Invalid input. Please enter a valid number.');
-}
+// let firstnum=prompt("Enter first number");
+// let secondnum=prompt("Enter second number");
+
+// if(!isNaN(firstnum) && !isNaN(secondnum)){
+// 	let sum=Number(firstnum)+Number(secondnum);
+// 	aler('The sum of ${firstnum} and ${secondnum} is ${sum}');
+// }
+// elseif(!isNaN(firstnum) || !isNaN(secondnum))
+// {
+// 	alert('Invalid input. Please enter a valid number.');
+// }
+// else{
+// 	alert('Invalid input. Please enter a valid number.');
+// }
