73636b97607f11a729f312a06fd0a3bea7b956f3
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-19 20:39:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let firstnum=prompt("Enter first number");
+let secondnum=prompt("Enter second number");
+
+if(!isNaN(firstnum) && !isNaN(secondnum)){
+	let sum=Number(firstnum)+Number(secondnum);
+	aler('The sum of ${firstnum} and ${secondnum} is ${sum}');
+}
+elseif(!isNaN(firstnum) && !isNaN(secondnum))
+{
+	alert('Invalid input. Please enter a valid number.');
+}
+else{
+	alert('Invalid input. Please enter a valid number.');
+}
